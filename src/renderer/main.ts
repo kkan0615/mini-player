98b42fdb9b052856d3df7daba090562eb6e6a268
kkan0615 <kkan0615@gmail.com>
2021-12-09 11:26:48 +0900

    :rocket: version 0.0.1a

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+/* Store */
+import { store } from '@/store'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app
+  .use(store)
+
+// /* Bootstrap */
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap'
+// /* Tailwind */
+// import '@/styles/tailwind.scss'
+// import '@/styles/index.scss'
