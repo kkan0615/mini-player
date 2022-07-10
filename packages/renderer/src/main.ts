71583094b74d9857b6cea5533f3f18179fb3053a
kkan0615 <kkan0615@gmail.com>
2022-07-10 19:07:18 -0400

    :zap: Feat: add quasar

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -3,7 +3,16 @@ import App from './App.vue'
 import router from '@/router'
 /* Add global styles */
 import '@/styles/index.scss'
+/* Add Quasar */
+import { Quasar } from 'quasar'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
 
 const app = createApp(App)
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
 app.use(router)
 app.mount('#app')
--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -4,9 +4,17 @@ import { builtinModules } from 'module'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: 'packages/renderer/src/styles/libs/quasar/variables.sass'
+    })
+  ],
   root: __dirname,
   base: process.env.IS_DEV !== 'true' ? './' : '/',
 
